9db8f850a3cad4b69e4c79810168d4c97317d4fd
MB-29 <55185203+MB-29@users.noreply.github.com>
2021-03-22 09:52:28 +0100

    mnist

--- predict_MNIST.py
+++ predict_MNIST.py
@@ -8,17 +8,15 @@ from nngp import NNGP
 
 n_train = 1000
 n_test = 200
-Ny, Nx = 28, 28
 
 sigma_eps = 0.1
-sigma_w_2 = 1.45
+sigma_w_2 = 1
 sigma_b_2 = 0.28
 L = 5
 
 data = MNIST('../../Data/MNIST/raw')
 data_vectors, labels = data.load_training()
 data_vectors, labels = np.array(data_vectors), np.array(labels)
-data_images = data_vectors.reshape((-1, Ny, Nx))
 print('Loaded MNIST')
 
 training_data = data_vectors[:n_train]
